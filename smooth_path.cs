35520baa1d90b6b1e6cd1780832093c7155277b2
Shattered MINT <shatteredmint@gmail.com>
2023-06-11 10:34:22 +0200

    imporve smoothing

--- smooth_path.cs
+++ smooth_path.cs
@@ -77,7 +77,7 @@ public partial class smooth_path : Path2D
 	{
 		Vector2 last = getPoint(index - 1);
 		Vector2 next = getPoint(index + 1);
-		Vector2 spline = last.DirectionTo(next) * Mathf.Max(SplineLength * ((next - last).Length() / 100), 5);
+		Vector2 spline = last.DirectionTo(next) * SplineLength * ((next - last).Length() > 450 ? 5 : 1);
 		return spline;
 	}
 
